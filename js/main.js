93487d996f7567498dce4063cd730e4f31911261
Erik-Hanson <ErikHanson135@gmail.com>
2021-02-09 13:54:46 -0800

    Updating site

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".navLink").click(function(e){
-        e.preventDefault(); // e is for event
-        var att = $(this).attr("href");
-        $('html,body').animate({scrollTop: $(att).offset().top},'slow');
-    });
-  });
